3dd9bf59c81ac959edd602aa04090373b6a38b4b
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-05-19 21:22:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/xvso/Objects/Game.java
@@ -0,0 +1,154 @@
+package com.example.xvso.Objects;
+
+public class Game {
+
+    // game status constants
+    public static final int STATUS_WAITING = 0;
+    public static final int STATUS_PLAYING = 1;
+    public static final int STATUS_FINISHED = 2;
+
+    private Board board;
+    private User host;
+    private User guest;
+    private int status;
+
+    private int picture;
+    private String gameNumber;
+    private String userName;
+
+    private String key;
+
+    private int acceptedRequest;
+
+    private String currentPlayer; // host or guest uid
+    private int gameResult; // 0 = initial state - 1 = host win - 2 = guest win - 3 = draw
+
+    // empty constructor
+    public Game() {
+
+    }
+
+    public Game(Board board, User host, User guest, int status, int picture, String gameNumber, String userName, String key, int acceptedRequest, String currentPlayer, int gameResult) {
+        this.board = board;
+        this.host = host;
+        this.guest = guest;
+        this.status = status;
+        this.picture = picture;
+        this.gameNumber = gameNumber;
+        this.userName = userName;
+        this.key = key;
+        this.acceptedRequest = acceptedRequest;
+        this.currentPlayer = currentPlayer;
+        this.gameResult = gameResult;
+    }
+
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(String currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    public int getGameResult() {
+        return gameResult;
+    }
+
+    public void setGameResult(int gameResult) {
+        this.gameResult = gameResult;
+    }
+
+    public Game(int picture, String gameNumber, String userName, int acceptedRequest) {
+        this.picture = picture;
+        this.gameNumber = gameNumber;
+        this.userName = userName;
+        this.acceptedRequest = acceptedRequest;
+    }
+
+    // constructor
+    public Game(Board board, User host, User guest, int status, int picture, String gameNumber, String userName, String key, int acceptedRequest) {
+        this.board = board;
+        this.host = host;
+        this.guest = guest;
+        this.status = status;
+        this.picture = picture;
+        this.gameNumber = gameNumber;
+        this.userName = userName;
+        this.key = key;
+        this.acceptedRequest = acceptedRequest;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public User getHost() {
+        return host;
+    }
+
+    public void setHost(User host) {
+        this.host = host;
+    }
+
+    public User getGuest() {
+        return guest;
+    }
+
+    public void setGuest(User guest) {
+        this.guest = guest;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getPicture() {
+        return picture;
+    }
+
+    public void setPicture(int picture) {
+        this.picture = picture;
+    }
+
+    public String getGameNumber() {
+        return gameNumber;
+    }
+
+    public void setGameNumber(String gameNumber) {
+        this.gameNumber = gameNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public int getAcceptedRequest() {
+        return acceptedRequest;
+    }
+
+    public void setAcceptedRequest(int acceptedRequest) {
+        this.acceptedRequest = acceptedRequest;
+    }
+}
+
+
---
+++ app/src/main/java/com/example/xvso/viewmodel/OnlineGameViewModel.java
@@ -0,0 +1,158 @@
+package com.example.xvso.viewmodel;
+
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+
+import com.example.xvso.GameDeserializer;
+import com.example.xvso.Objects.Board;
+import com.example.xvso.Objects.Game;
+import com.example.xvso.eventobserver.Event;
+import com.example.xvso.firebaseutils.FirebaseQueryLiveData;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class OnlineGameViewModel extends BaseViewModel {
+
+    private final String LOG_TAG = this.getClass().getSimpleName();
+    private final String MULTIPLAYER = "multiplayer";
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private DatabaseReference query;
+
+    private MutableLiveData<ArrayList<Integer>> boardLiveData = new MutableLiveData<>();
+    public LiveData<Game> gameLiveData;
+    private Game game = new Game();
+
+    private String gameID = "";
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public OnlineGameViewModel(String gameID) {
+        this.gameID = gameID;
+
+          if (gameID != null) {
+
+            query = FirebaseDatabase.getInstance().getReference(MULTIPLAYER).child((gameID));
+
+            FirebaseQueryLiveData resultLiveData = new FirebaseQueryLiveData(query);
+            gameLiveData = Transformations.map(resultLiveData, new GameDeserializer());
+
+        }
+    }
+
+    public void saveCellToFirebase(int position) {
+        game = gameLiveData.getValue();
+        if (auth.getCurrentUser().getUid().equals(game.getHost().getUID())) {
+            game.getBoard().getCells().get(position).setTag(1);
+
+
+        } else {
+            game.getBoard().getCells().get(position).setTag(2);
+
+        }
+
+        if (game.getCurrentPlayer().equals(game.getHost().getUID())) {
+            game.setCurrentPlayer(game.getGuest().getUID());
+        } else {
+            game.setCurrentPlayer(game.getHost().getUID());
+        }
+
+        query
+                .setValue(game)
+
+        ;
+
+        checkForWin();
+    }
+
+    public boolean checkRows() {
+
+        int team;
+
+        if (auth.getCurrentUser().getUid().equals(game.getHost().getUID())) {
+            team = 1;
+        } else {
+            team = 2;
+        }
+
+            Board board = game.getBoard();
+
+        if (board.getCells().get(0).getTag() == team && board.getCells().get(1).getTag() == team && board.getCells().get(2).getTag() == team) {
+            topHorizontalLine.setValue(true);
+            return true;
+        } else if (board.getCells().get(3).getTag() == team && board.getCells().get(4).getTag() == team && board.getCells().get(5).getTag() == team) {
+            centerHorizontal.setValue(true);
+            return true;
+        } else if (board.getCells().get(6).getTag() == team && board.getCells().get(7).getTag() == team && board.getCells().get(8).getTag() == team) {
+            bottomHorizontal.setValue(true);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkColumns() {
+
+        int team;
+
+        if (auth.getCurrentUser().getUid().equals(game.getHost().getUID())) {
+            team = 1;
+        } else {
+            team = 2;
+        }
+
+        Board board = game.getBoard();
+        if (board.getCells().get(0).getTag() == team && board.getCells().get(3).getTag() == team && board.getCells().get(6).getTag() == team) {
+            leftVertical.setValue(true);
+            return true;
+        } else if (board.getCells().get(1).getTag() == team && board.getCells().get(4).getTag() == team && board.getCells().get(7).getTag() == team) {
+            centerVertical.setValue(true);
+            return true;
+        } else if (board.getCells().get(2).getTag() == team && board.getCells().get(5).getTag() == team && board.getCells().get(8).getTag() == team) {
+            rightVertical.setValue(true);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkDiagonals() {
+
+        int team;
+
+        if (auth.getCurrentUser().getUid().equals(game.getHost().getUID())) {
+            team = 1;
+        } else {
+            team = 2;
+        }
+        Board board = game.getBoard();
+
+
+        if (board.getCells().get(0).getTag() == team && board.getCells().get(4).getTag() == team && board.getCells().get(8).getTag() == team) {
+            leftRightDiagonal.setValue(true);
+            return true;
+        } else if (board.getCells().get(2).getTag() == team && board.getCells().get(4).getTag() == team && board.getCells().get(6).getTag() == team) {
+            rightLeftDiagonal.setValue(true);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkForWin() {
+        if (checkRows() || checkColumns() || checkDiagonals()) {
+            setGameOver(true);
+            return true;
+        } else {
+            setGameOver(false);
+            return false;
+        }
+    }
+}
