f38c6e3762f3a88d105ef412e34f0a9ed5c0dc6e
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-06-04 21:24:17 +0200

    04.06.2020 SinglePlayer

---
+++ app/src/main/java/com/example/xvso/viewmodel/SinglePlayerViewModel.java
@@ -0,0 +1,194 @@
+package com.example.xvso.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.xvso.object.Board;
+import com.example.xvso.object.Cell;
+import com.example.xvso.object.Game;
+
+import com.example.xvso.object.WinningLines;
+
+public class SinglePlayerViewModel extends BaseViewModel {
+
+    private Game game = new Game();
+    private MutableLiveData<Game> gameLiveData;
+
+    private static final String PLAYER_X ="playerX";
+    private static final String PLAYER_O ="playerO";
+
+    private static final int TEAM_X = 1;
+    private static final int TEAM_O = 2;
+
+    public void saveCell(int position) {
+        game = gameLiveData.getValue();
+        if (game != null) {
+
+            if (game.getCurrentPlayer().equals(PLAYER_X)) {
+                game.getBoard().getCells().get(position).setTag(1);
+            } else {
+                game.getBoard().getCells().get(position).setTag(2);
+
+                if (game.getCurrentPlayer().equals(PLAYER_X)) {
+                    game.setCurrentPlayer(PLAYER_O);
+                } else {
+                    game.setCurrentPlayer(PLAYER_X);
+                }
+                gameLiveData.setValue(game);
+            }
+            checkForWin();
+        }
+    }
+
+    public boolean checkRows() {
+
+        int team;
+
+        if (game.getCurrentPlayer().equals(PLAYER_X))  {
+            team = TEAM_X;
+        } else {
+            team = TEAM_O;
+        }
+
+        Board board = game.getBoard();
+
+        if (board.getCells().get(0).getTag() == team && board.getCells().get(1).getTag() == team && board.getCells().get(2).getTag() == team) {
+            game.getWinningLines().setTopHorizontalLine(1);
+            gameLiveData.setValue(game);
+            return true;
+        } else if (board.getCells().get(3).getTag() == team && board.getCells().get(4).getTag() == team && board.getCells().get(5).getTag() == team) {
+            game.getWinningLines().setCenterHorizontal(1);
+            gameLiveData.setValue(game);
+            return true;
+        } else if (board.getCells().get(6).getTag() == team && board.getCells().get(7).getTag() == team && board.getCells().get(8).getTag() == team) {
+            game.getWinningLines().setBottomHorizontal(1);
+            gameLiveData.setValue(game);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+    public boolean checkColumns() {
+
+        int team;
+
+        if (game.getCurrentPlayer().equals(PLAYER_X))  {
+            team = TEAM_X;
+        } else {
+            team = TEAM_O;
+        }
+
+        Board board = game.getBoard();
+        if (board.getCells().get(0).getTag() == team && board.getCells().get(3).getTag() == team && board.getCells().get(6).getTag() == team) {
+            game.getWinningLines().setLeftVertical(1);
+            gameLiveData.setValue(game);
+            return true;
+        } else if (board.getCells().get(1).getTag() == team && board.getCells().get(4).getTag() == team && board.getCells().get(7).getTag() == team) {
+            game.getWinningLines().setCenterVertical(1);
+            gameLiveData.setValue(game);
+            return true;
+        } else if (board.getCells().get(2).getTag() == team && board.getCells().get(5).getTag() == team && board.getCells().get(8).getTag() == team) {
+            game.getWinningLines().setRightVertical(1);
+            gameLiveData.setValue(game);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public boolean checkDiagonals() {
+
+        int team;
+
+        if (game.getCurrentPlayer().equals(PLAYER_X))  {
+            team = TEAM_X;
+        } else {
+            team = TEAM_O;
+        }
+
+        Board board = game.getBoard();
+
+        if (board.getCells().get(0).getTag() == team && board.getCells().get(4).getTag() == team && board.getCells().get(8).getTag() == team) {
+            game.getWinningLines().setLeftRightDiagonal(1);
+            gameLiveData.setValue(game);
+            return true;
+        } else if (board.getCells().get(2).getTag() == team && board.getCells().get(4).getTag() == team && board.getCells().get(6).getTag() == team) {
+            game.getWinningLines().setRightLeftDiagonal(1);
+            gameLiveData.setValue(game);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkForWin() {
+        game = gameLiveData.getValue();
+        if (checkRows() || checkColumns() || checkDiagonals()) {
+            if (auth.getCurrentUser().getUid().equals(game.getHost().getUID())) {
+
+                game.setGameResult(1);
+                game.setHostScore(game.getHostScore() + 1);
+                gameLiveData.setValue(game);
+
+            } else {
+
+                game.setGameResult(2);
+                game.setGuestScore(game.getGuestScore() + 1);
+                gameLiveData.setValue(game);
+
+            }
+            return true;
+        } else {
+            for (Cell cell: game.getBoard().getCells()) {
+                if (cell.getTag() == 0) {
+                    return false; // code exits if it finds an empty cell
+                }
+            }
+            game.setGameResult(3); // draw
+            gameLiveData.setValue(game);
+            return false;
+        }
+    }
+
+
+    public void gameEnded(){
+        isGameInProgress.postValue(false);
+        updateScore();
+    }
+
+    public void newRound() {
+
+        game = gameLiveData.getValue();
+        game.setBoard(new Board());
+        game.setWinningLines(new WinningLines());
+        game.setGameResult(0);
+        game.setRoundCount(game.getRoundCount() + 1);
+        gameLiveData.setValue(game);
+    }
+
+    public void resetGame(){
+
+        game = gameLiveData.getValue();
+        game.setBoard(new Board());
+        game.setWinningLines(new WinningLines());
+        game.setGameResult(0);
+        game.setRoundCount(0);
+        gameLiveData.setValue(game);
+        resetScore();
+    }
+
+    public void resetScore() {
+        game.setHostScore(0);
+        game.setGuestScore(0);
+    }
+
+    public void timeUp() {
+        game = gameLiveData.getValue();
+        game.setGameResult(3);
+        gameLiveData.setValue(game);
+    }
+}
